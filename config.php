97f905b3b4278b71bdda428e41514e2e00ac499d
Tul19 <154302410+Tul19@users.noreply.github.com>
2024-03-13 14:46:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 define('servername', 'localhost');  // Replace with your database host (e.g., localhost)
-define('username', 'id21922441_userinfo');  // Replace with your database username
-define('password', 'Q464z!YWP!r>VWG8');  // Replace with your database password
-define('dbname', 'id21922441_userdb');  // Replace with your database name
+define('username', 'id21952530_male');  // Replace with your database username
+define('password', 'Checkin12345.');  // Replace with your database password
+define('dbname', 'id21952530_checkin');  // Replace with your database name
 
 // Create connection
 $mysqli = new mysqli(servername, username, password, dbname);
